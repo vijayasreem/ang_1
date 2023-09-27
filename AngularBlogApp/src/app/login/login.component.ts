1c0ea2866563821f07cbfbe698e5b522ffc5a659
dotnet-code <@dotnet-code>
2023-09-27 13:05:14 +0000

    commit from code202309270514

---
+++ AngularBlogApp/src/app/login/login.component.ts
@@ -0,0 +1,34 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(8)])
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      // Perform API call for login functionality
+    }
+  }
+
+  get email() {
+    return this.loginForm.get('email');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
+}
